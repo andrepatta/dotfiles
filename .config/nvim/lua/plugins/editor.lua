9b25e9b51186ee0f0807691f6f007c8d519191b9
André Patta <andre_luis@outlook.com>
2024-06-01 19:09:48 -0300

    nvim update nvim-cpm to go along with the copilot changes

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -54,6 +54,7 @@ return {
 
   {
     "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
       "hrsh7th/cmp-emoji",
       {
@@ -87,11 +88,24 @@ return {
         require("copilot_cmp.comparators").prioritize,
       })
 
-      -- opts.experimental.ghost_text = false
+      opts.experimental.ghost_text = false
 
       local cmp = require("cmp")
+      -- opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      --   ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      -- })
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<CR>"] = cmp.mapping({
+          i = function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else
+              fallback()
+            end
+          end,
+          s = cmp.mapping.confirm({ select = true }),
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
       })
     end,
   },
