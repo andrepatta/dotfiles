bb0499af9b4a05d7553fba0145e0006001059e4f
André Patta <andre_luis@outlook.com>
2024-07-19 05:15:40 -0300

    fix templ formatting. use lsp instead of templ fmt

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,26 +6,7 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_custom_" .. name, { clear = true })
 end
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = augroup("templ_format_on_save"),
-  pattern = "*.templ",
-  callback = function()
-    if vim.bo.filetype == "templ" then
-      local bufnr = vim.api.nvim_get_current_buf()
-      local filename = vim.api.nvim_buf_get_name(bufnr)
-      local cmd = "templ fmt " .. vim.fn.shellescape(filename)
-
-      vim.fn.jobstart(cmd, {
-        on_exit = function()
-          -- Reload the buffer only if it's still the current buffer
-          if vim.api.nvim_get_current_buf() == bufnr then
-            vim.cmd("e!")
-          end
-        end,
-      })
-    end
-  end,
-})
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = LazyVim.lsp.format })
 
 -- Workaround to get Raycast AI commands to identify selected text in Neovim
 vim.api.nvim_create_autocmd("FocusLost", {
