4bb8f9db6beef192e82fac1db91d3291951ffa56
André Patta <andre_luis@outlook.com>
2024-08-01 13:16:42 -0300

    nvim: fix cmp autocomplete with CR

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -101,17 +101,18 @@ return {
 
       local cmp = require("cmp")
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        -- ["<CR>"] = cmp.mapping({
-        --   i = function(fallback)
-        --     if cmp.visible() and cmp.get_active_entry() then
-        --       cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-        --     else
-        --       fallback()
-        --     end
-        --   end,
-        --   s = cmp.mapping.confirm({ select = true }),
-        --   c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-        -- }),
+        ["<CR>"] = cmp.mapping({
+          i = function(fallback)
+            if cmp.visible() and cmp.get_active_entry() then
+              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            else
+              cmp.close()
+              fallback()
+            end
+          end,
+          s = cmp.mapping.confirm({ select = true }),
+          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.confirm({ select = true })
